191d4436c7996e643e94d2498717ae3f56ddc60d
ntello <team@unarix.com.ar>
2020-08-20 01:20:08 -0300

    initial

---
+++ Models/menu.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace QRcode_menu.Models
+{
+    public class menuItem
+    {
+        public string titulo_es { get; set; }
+        public string titulo_en { get; set; }
+        public string titulo_pt { get; set; }        
+    }
+
+    public class menuDta
+    {
+        public List<menuItem> items { get; set; }
+        public string Nombre { get; set; }
+        
+    }
+}
---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.IO;
+using QRcode_menu.Models;
+
+namespace QRcode_menu.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            string id = Request.Query["id"];
+            if(id != null)
+            {
+                //Obtengo el directorio para el archivo json
+                var service = HttpContext.RequestServices.GetService(typeof(Microsoft.AspNetCore.Hosting.IHostingEnvironment)) as Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+                string folderName = "json/";
+                string webRootPath = service.WebRootPath;
+                string Path = System.IO.Path.Combine(webRootPath, folderName);
+
+                // Cargo el json en la entidad
+                menuDta sf = JsonConvert.DeserializeObject<menuDta>(System.IO.File.ReadAllText(Path + @"/menu_" + id + ".json"));
+                
+            }
+        }
+
+        public void OnPost()
+        {
+
+        }
+
+    }
+}
