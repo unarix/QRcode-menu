5fa076014c5161858024204d2856d24f4b289b7b
ntello <team@unarix.com.ar>
2020-08-28 01:31:55 -0300

    if exist order

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -27,13 +27,13 @@ namespace QRcode_menu.Pages
             pedidos = "";
             string id = Request.Query["id"];
             string sector = "1"; // Request.Query["sec"];
-            string pedidoExistente;
+            string pedido_reciente = "";
 
             if(id != null)
             {
-                bool isExist = memoryCache.TryGetValue("mesa_" + id, out pedidoExistente);
+                bool reciente = memoryCache.TryGetValue("mesa_" + id, out pedido_reciente);
 
-                if(!isExist || pedidoExistente.Equals(""))
+                if(!existePedido(id) && !reciente)
                 {
                     //Obtengo el directorio para el archivo json
                     var service = HttpContext.RequestServices.GetService(typeof(Microsoft.AspNetCore.Hosting.IHostingEnvironment)) as Microsoft.AspNetCore.Hosting.IHostingEnvironment;
@@ -49,11 +49,11 @@ namespace QRcode_menu.Pages
                 {
                     string pedido = @"<div class='alert alert-success' role='alert'> 
                     <h4>Ups!!!</h4>
-                    <p class='text-success'>Ya existe un pedido para tu mesa de hace menos de 5 minutos (Nro: " + id + ") </p>";
+                    <p class='text-success'>Queres hacer algún cambio en tu pedido? informa al asistente de la sala (estas en la mesa Nro: " + id + ") </p>";
                     pedido += "<b>";
-                    pedido += pedidoExistente;
+                    pedido += pedido_reciente;
                     pedido += @"</b> 
-                    <h6>Si necesitas hacer alguna modificacion solicítelo al personal de la sala</h6></span>
+                    <h6>Ya estamos preparando todo para que pronto este en tu mesa</h6></span>
                     </div>";
 
                     pedidos = pedido;
@@ -86,23 +86,27 @@ namespace QRcode_menu.Pages
             try{
                 string message = Request.Form[nameof(pedidos)];;
                 string[] messageArray = message.Split("#");
-                pedido = @"<div class='alert alert-success' role='alert'> 
+                
+                //Aqui se debe llamar a la funcion que enviá el email; en caso de fallo debemos avisar que no se pudo realizar.
+                if (guardarPedido(messageArray, id)){
+
+                    pedido = @"<div class='alert alert-success' role='alert'> 
                     <h4>Todo salio muy bien!</h4>
                     <p class='text-success'>Ya estamos preparando el pedido para tu mesa (Nro: " + id + ") </p>";
 
-                pedido += "<b>";
-                foreach(string m in messageArray){
-                    pedido += m + "<br>";
+                    pedido += "<b>";
+                    foreach(string m in messageArray){
+                        pedido += m + "<br>";
+                    }
+                    pedido += @"</b> 
+                        <h6>Si necesitas hacer alguna modificacion solicítelo al personal de la sala</h6></span>
+                        </div>";
                 }
-                pedido += @"</b> 
-                    <h6>Si necesitas hacer alguna modificacion solicítelo al personal de la sala</h6></span>
-                    </div>";
-
-                //Aqui se debe llamar a la funcion que enviá el email; en caso de fallo debemos avisar que no se pudo realizar.
-                if (!guardarPedido(messageArray, id)){
+                else
+                {
                     pedido = 
-                        @"<div class='alert alert-danger' role='alert'> 
-                            <h2>Uy! algo paso cuando enviábamos tu pedido, por favor contacta al personal de la sala.</h2>
+                        @"<div class='alert alert-success' role='alert'> 
+                            <h6>Ya tenes un pedido previo, pronto estara listo y en tu mesa.</h6>
                         </div>";
                 }               
             }
@@ -114,9 +118,27 @@ namespace QRcode_menu.Pages
             pedidos = pedido;
         }
 
+        private bool existePedido(string id)
+        {
+            orderDta orders = new orderDta(); 
+
+            // Abro el objeto json e inserto el nuevo pedido
+            var service = HttpContext.RequestServices.GetService(typeof(Microsoft.AspNetCore.Hosting.IHostingEnvironment)) as Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+            string folderName = "json/";
+            string webRootPath = service.WebRootPath;
+            string Path = System.IO.Path.Combine(webRootPath, folderName);
+
+            // Cargo el json en la entidad
+            orderDta od = JsonConvert.DeserializeObject<orderDta>(System.IO.File.ReadAllText(Path + @"/orders.json"));
+
+            var o = od.order.Where(x => x.mesa == id);
+
+            return (o.Count() > 0);
+        }
+
         private bool guardarPedido(string[] pedidos, string id)
         {
-            try{        
+            try{
                 string pedido = "";
                 orderDta orders = new orderDta(); 
 
@@ -129,6 +151,7 @@ namespace QRcode_menu.Pages
                 // Cargo el json en la entidad
                 orderDta od = JsonConvert.DeserializeObject<orderDta>(System.IO.File.ReadAllText(Path + @"/orders.json"));
                 orders = od;
+
                 orders order = new orders();
                 order.mesa = id;
                 order.fecha_hora = DateTime.Now.ToString();
@@ -149,7 +172,7 @@ namespace QRcode_menu.Pages
                 // Uso el inmemory para guardar el pedido de esa mesa y que no vuelvan a hacerlo hasta 3000 seg despues.
                 var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(300));
                 memoryCache.Set("mesa_" + id, pedido, cacheEntryOptions);
-
+                
                 return true;
             }
             catch(Exception ex)
